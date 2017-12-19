5281432194324151a14a0cad2dc95ccd837243d5
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2017-12-19 18:28:34 +0000

    GREEN file moving works

---
+++ scripts/triesSimProxyOut.R
@@ -0,0 +1,21 @@
+
+wd <- "~/shiny-concurrency/shiny-proxy-tests-for-code"
+setwd(wd)
+system("make clean && make do")
+outDir    <- file.path(wd, "out")
+
+create_simOut <- function(n) {
+  SimOut(poll       = NA,
+         outdir     = file.path(outDir, as.character(n)),
+         parameters = NA,
+         command    = "ls a b c")
+}
+
+
+simOutLs  <- map(.x = 1:10
+                 , .f = create_simOut)
+
+proxyOutLs <- SimProxyOutLs(simOutLs)
+
+print(listSimOutFiles.SimProxyOutLs(proxyOutLs))
+moveFiles.SimProxyOutLs(proxyOutLs)
