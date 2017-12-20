70aa17b71dad189d2c66e06de37c669222e87d9e
Andrea Melloncelli <andrea.melloncelli@gmail.com>
2017-12-20 14:46:56 +0000

    dockerApi working (no seriality)

--- scripts/dockerApi.R
+++ scripts/dockerApi.R
@@ -16,42 +16,100 @@ docker_kill <- function(container) {
 	kill_cmd <- paste0("docker kill ", container$hash)
 	system(kill_cmd)
 }
+docker_kill_all <- function() {
+  kill_cmd <- "docker kill $(docker ps -q) "
+  system(kill_cmd)
+}
 browser_get <- function(id, port = 4445L) {
 	mybrowser <- remoteDriver(remoteServerAddr = "127.0.0.1",
 														port = port)
 	mybrowser
 }
-container_get <- function(mybrowser) {
+container_get <- function(mybrowser, proxy_server = "ec2-107-23-143-101.compute-1.amazonaws.com", times = 1) {
 	mybrowser$open()
-	mybrowser$navigate("http://ec2-52-201-221-131.compute-1.amazonaws.com:8080/app/test_app")
-	# mybrowser$getPageSource()[[1]] %>% 
-	# 	htmlParse
-	Sys.sleep(3)
-	str <- mybrowser$getPageSource()[[1]] %>% 
-		strsplit(split = "\n") %>% 
-		.[[1]] %>% 
-		.[81]
+	str <- character()
+	for (i in 1:times) {
+	  mybrowser$deleteAllCookies()
+	  mybrowser$navigate(paste0("http://", proxy_server, ":8080/app/test_app"))
+	  # mybrowser$getPageSource()[[1]] %>%
+	  # 	htmlParse
+	  Sys.sleep(10) #3
+	  buf <- mybrowser$getPageSource()[[1]] %>%
+	    strsplit(split = "\n") %>%
+	    .[[1]] %>%
+	    .[81]
+	  str <- c(str, buf)
+	}
 	mybrowser$close()
 	str
 }
 
-url_get <- function(id, wait_container = 5) {
-	port <- 4445L + id
-	docker_container <- docker_run(port = port)
-	Sys.sleep(wait_container)
-	mybrowser <- browser_get(id, port = docker_container$port)
-	web_route <- container_get(mybrowser)
-	docker_kill(docker_container)
-	web_route
+url_get <- function(id, wait_container = 5, proxy_server = "ec2-107-23-143-101.compute-1.amazonaws.com", times = 1) {
+  port <- 4445L + id
+  docker_container <- docker_run(port = port)
+  Sys.sleep(wait_container)
+  mybrowser <- browser_get(id, port = docker_container$port)
+  web_route <- container_get(mybrowser, proxy_server = proxy_server, times = times)
+  docker_kill(docker_container)
+  web_route
 }
-url_get(id = 1)
+
+containerNames_get <- function(containers) {
+  flat_conteiners <- purrr::flatten_chr(containers)
+  clean_vec_1 <- grep(pattern = "    <iframe width=\"100%\" src=\"\""
+                    , x = flat_conteiners
+                    , value = TRUE
+                    , invert = TRUE)
+  clean_vec_2 <- grep(pattern = "Error in checkError\\(res\\) : \n  Undefined error in httr call. httr output:.*"
+                    , x = clean_vec_1
+                    , value = TRUE
+                    , invert = TRUE)
+  name_vec <- gsub(pattern = "    <iframe width=\"100%\" src=\"/(.*)/\" id=\"shinyframe\" style=\"height: 688px;\"></iframe>"
+                   , replacement = "\\1"
+                   , x = clean_vec_2)
+  name_vec
+}
+
+# conf_var ----------------------------------------------------------------
+
+proxy_server <- "ec2-35-153-83-243.compute-1.amazonaws.com"
+
+# simple_example ----------------------------------------------------------
+container <- docker_run(4445L)
+browsers <- lapply(1:2, browser_get, port = container$port)
+browsers[[1]]$open()
+browsers[[1]]$deleteAllCookies()
+browsers[[1]]$getSessions() %>% str
+browsers[[1]]$navigate(paste0("http://", proxy_server, ":8080/app/test_app"))
+Sys.sleep(10)
+(str <- browsers[[1]]$getPageSource()[[1]] %>%
+  strsplit(split = "\n") %>%
+  .[[1]] %>%
+  .[81])
+
+browsers[[1]]$close()
+docker_kill(container)
+
+# api_example -------------------------------------------------------------
+
+url_get(id = 1, proxy_server = proxy_server)
+
+
+# concurrent_example ------------------------------------------------------
 
 library(parallel)
-browsers <- lapply(1:2, browser_get)
-t <- system.time(containers <- mclapply(browsers, container_get))
-t
-t2 <- system.time(containers_2 <- mclapply(mc.cores = 40, 1:20, url_get))
-containers_2
+#
+# t <- system.time(containers <- mclapply(browsers, container_get))
+# t
+
+n_cores <- 20
+times <- 1
+docker_kill_all()
+t2 <- system.time(containers_2 <- mclapply(mc.cores = n_cores, 1:n_cores, url_get, wait_container = 10, proxy_server = proxy_server, times = times))
+(nam <- containerNames_get(containers_2))
 t2
-containers
-container_get(mybrowser)
+containerStrings <- nam[!is.na(nam)]
+# other -------------------------------------------------------------------
+
+
+
